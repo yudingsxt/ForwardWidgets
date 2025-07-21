05a92eeecc7d61d27427094d02d6337a552311e9
Asaki <37176460+yudingsxt@users.noreply.github.com>
2025-07-22 00:52:41 +0800

    Update javrate.js

--- Widgets/javrate.js
+++ Widgets/javrate.js
@@ -922,7 +922,7 @@ var WidgetMetadata = {
 };
 
 
-const ARTIST_MAP_REMOTE_URL = "https://raw.githubusercontent.com/quantumultxx/ForwardWidgets/refs/heads/main/Widgets/javrate_actors.json";
+const ARTIST_MAP_REMOTE_URL = "https://raw.githubusercontent.com/yudingsxt/ForwardWidgets/refs/heads/main/Widgets/javrate_actors.json";
 let artistMapCache = null;
 let artistMapCacheTime = 0;
 const CACHE_DURATION = 24 * 60 * 60 * 1000;
